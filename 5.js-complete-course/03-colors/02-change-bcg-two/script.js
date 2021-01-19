307aa103193aa43c2d928535383cdcd7ffba1769
Paul Sempels <paulsempels@gmail.com>
2021-01-19 10:30:10 +0100

    finished the exercises up to C.3

--- 5.js-complete-course/03-colors/02-change-bcg-two/script.js
+++ 5.js-complete-course/03-colors/02-change-bcg-two/script.js
@@ -12,5 +12,7 @@
 (function() {
 
     // your code here
-
+    document.getElementById("run").addEventListener("click", function() {
+        document.body.style.backgroundColor = document.getElementById("color").value;
+    });
 })();
--- 5.js-complete-course/03-colors/03-change-bcg-three/script.js
+++ 5.js-complete-course/03-colors/03-change-bcg-three/script.js
@@ -12,5 +12,16 @@
 (function() {
 
     // your code here
+    document.getElementById("run").addEventListener("click", function() {
 
+    let test = Math.random()*87654321;
+    let test2 = Math.floor(test);
+    let test3 = test2.toString(16)
+    let randomColor = Math.floor(Math.random()*8654321).toString(16); // the *8654321 is random to get a big(ger) number
+    console.log(test);
+    console.log(test2);
+    console.log(test3);
+    console.log(randomColor); // color code missing hash
+    document.body.style.backgroundColor = ('#'+randomColor);
+    });
 })();
